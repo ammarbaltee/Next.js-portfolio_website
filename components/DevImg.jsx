fa97c4a098d856df60262690db93f54a964c858b
Ammar.yasir <m.ammar@bondwest.co.uk>
2024-10-14 22:41:15 +0500

    image optimized in two components

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
-
+import useWindowSize from '../hooks/useWindowSize';
 
 const DevImg = ({containerStyles, ImgSrc}) => {
+  const { width } = useWindowSize();
   return (
     <div className={`${containerStyles}`}>
-        <Image src={ImgSrc} fill priority alt='' />
+      <Image 
+        src={ImgSrc} 
+        width={505}
+        height={505}
+        loading={width < 768 ? "lazy" : "eager"}
+        decoding="async"
+        priority={width >= 768} // Set priority for larger screens 
+      />
     </div>
   )
 }
